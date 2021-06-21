7f7b4da53f78df1ed1b256e05c47101e2e542dbc
Trace <tracedelange@me.com>
2021-06-21 15:30:18 -0500

    Lots of progress made. Css looks pretty awesome, got eater aging and reproduction working as well as some basic stats. Still some strange bugs about

--- javascripts/index.js
+++ javascripts/index.js
@@ -2,7 +2,7 @@ const width = document.body.clientWidth
 const height = document.body.clientHeight
 // const width = (window.screen.width)*.95
 // const height = (window.screen.height)*.8
-const gridScale = 50
+const gridScale = 30
 const margin = 1
 const html = document.getElementsByTagName('html')[0]
 const lineSpacing = Math.floor(height/gridScale)
@@ -10,7 +10,7 @@ const lineSpacing = Math.floor(height/gridScale)
 const numRows = gridScale
 const numCols = (width/lineSpacing) - 1
 
-const frameRate = 1000/20
+const frameRate = 1000/15
 
 //1980x1080 most common screen size apparently 
 
@@ -18,13 +18,15 @@ const green = "93E9BE"
 const background = "E2FAB5"
 
 //Creature constants
-const greenGrowthRate = .1
+const greenGrowthRate = .03
 
 const eaterPerception = 10 //measure of how many blocks the eaters can scan in search of a green
 const eaterScale = 1
 const eaterLifespan  = 200 //measure of how many frames an eater survives before death
 const eaterDeathChance = .01
-const eaterReproductionRate = .5
+const eaterReproductionRate = .01
+const eaterGestationPeriod = 50
+const eaterNutrientRequirement = 50
 
 
 // DOM item declarations
@@ -38,13 +40,15 @@ const pauseButton = document.getElementById('pause')
 const resumeButton = document.getElementById('resume')
 const resetButton = document.getElementById('reset')
 
-
-
-
+const greenPop = document.getElementById('green-pop')
+const eaterPop = document.getElementById('eater-pop')
+const greenEaterRatio = document.getElementById('green-eater-ratio')
+const averageGen = document.getElementById('average-eater-generation')
 
 
 const eaters = [];
 const greens = [];
+const messages = [];
 
 //Simulation running bool
 let running = false
@@ -55,18 +59,38 @@ let app = new PIXI.Application({
     // height: height,
     width: width, 
 	height: height,
-	autoResize: false,
+	autoResize: true,
     backgroundColor: 0xE2FAB5,
     // resizeTo: window
 
     resolution: window.devicePixelRatio || 1
 });
 
+let messageStyle = new PIXI.TextStyle({
+    fontFamily: "Times New Roman",
+    fontSize: 36,
+    fill: "white",
+    stroke: 'black',
+    strokeThickness: 2,
+    dropShadow: true,
+    // dropShadowColor: "#000000",
+    dropShadowBlur: 4,
+    // dropShadowAngle: Math.PI / 6,
+    // dropShadowDistance: 6,
+  });
+
+// let bg = new PIXI.Sprite(PIXI.loader.resources["./assets/images/sand.png"].texture);    
+// bg.width = width
+// bg.height = height
+
+// app.stage.addChild(bg);
+
 // const eaterTexture = PIXI.Texture.from('./assets/images/eater.png');
 
 PIXI.Loader.shared
 .add("./assets/images/eater.png")
 .add("./assets/images/green.png")
+.add("./assets/images/dead_eater.png")
 .load(function(){
       console.log('resources loaded')
   })
@@ -102,6 +126,16 @@ const drawGrid = function(){
     }
 }
 
+// const drawBackgroundSprite = () => {
+
+//     let bg = new PIXI.Sprite(PIXI.loader.resources["./assets/images/sand.png"].texture);    
+//     bg.width = width
+//     bg.height = height
+    
+//     app.stage.addChild(bg);
+    
+// }
+
 //Grid manipulation
 const generateGrid = function(gridScale){
     const grid = []
@@ -131,21 +165,51 @@ const setGridArray = (x,y,index,value) => {
     grid[x][y][index] = value
 }
 
-const scanArea = (id, range, target) => {
+const scanAreaForNewEater = (id) => {
+    let pX = parseInt(id.split(',')[0])
+    let pY = parseInt(id.split(',')[1])
+
+
+    for (let xx = -1; xx < 2; xx++){
+        for (let yy = -1; yy < 2; yy++){
+            // console.log(xx,yy)
+            // console.log(grid[xx+pX][yy+pY])
+
+            if (grid[xx+pX][yy+pY] !== undefined){
+                if (grid[xx+pX][yy+pY][0] !== 2){
+                    let cX = xx+pX
+                    let cY = yy+pY
+    
+                    return [cX, cY]
+                }
+            }
+        }
+    }
+
+
+}
+
+
+const scanAreaForGreens = (id, range) => {
     //Alternate approach: Filter through the list of greens and determine which are closest. OR Use .find to determine the first green that satisfies the callback
     
     let bx = parseInt(id.split(',')[0])
     let by = parseInt(id.split(',')[1])
     
-    return greens.find(function(green){
+    let targets = greens.filter(function(green){
         let gx = parseInt(green.id.split(',')[0])
         let gy = parseInt(green.id.split(',')[1])
         // console.log(green.id, id)
         // console.log(Math.abs(bx-gx), Math.abs(by-gy))
         if (Math.abs(bx-gx) < range && Math.abs(by-gy) < range){
-           return gx,gy
+            
+            return green
         }
     })
+
+    if (targets !== undefined) {
+        return targets //[Math.floor(Math.random()*targets.length)]
+    }
 }
 
 //Green Functions
@@ -165,6 +229,7 @@ const drawGreenSprite = (x,y) => {
 
     app.stage.addChild(green);
 }
+
 
 const populateGreens = (n) => {
     for (let i = 0; i < n; i++){
@@ -227,11 +292,25 @@ const drawEater = (x,y) => {
     eater.y = (lineSpacing+margin) + ((lineSpacing)*(y-1)) + margin;
 
     eater.age = 0
+    eater.nutrients = 0
 
     eater.id = `${x},${y}`
     
     app.stage.addChild(eater);
     eaters.push(eater)
+}
+
+const drawDeadEater = (x,y) => {
+    // console.log('running draw dead eater')
+    let deadEater = new PIXI.Sprite(PIXI.loader.resources["./assets/images/dead_eater.png"].texture)
+
+    deadEater.width = (lineSpacing - margin)*eaterScale
+    deadEater.height = (lineSpacing - margin)*eaterScale
+    
+    deadEater.x = (lineSpacing+margin) + ((lineSpacing)*(x-1));
+    deadEater.y = (lineSpacing+margin) + ((lineSpacing)*(y-1)) + margin;
+
+    app.stage.addChild(deadEater)
 }
 
 const manageAge = (eater) => {
@@ -248,9 +327,10 @@ const manageAge = (eater) => {
 
             grid[X][Y][0] = 0
             eaters.splice(eaterIndex, 1)
-
-            
             app.stage.removeChild(eater)
+
+            drawDeadEater(X,Y)
+
         }
 
     }
@@ -306,7 +386,7 @@ const moveEater = (eater) => {
 
 
     //Quick test to determine if destination location is not out of upper-bounds
-    if (x+xMod > grid.length -1 || y+yMod > grid.length-1){
+    if (x+xMod > grid.length -1 || y+yMod > grid[0].length-1){
         xMod = 0
         yMod = 0
     }
@@ -321,6 +401,12 @@ const moveEater = (eater) => {
         xMod = 0
     }
 
+    //Eater remains static if gestating
+    if (eater.gestation !== undefined) {
+        xMod = 0
+        yMod = 0
+    }
+
 
     //update the eaters X and Y position and then update the ID to reflect its position
     
@@ -332,6 +418,7 @@ const moveEater = (eater) => {
     
     if (grid[x+xMod][y+yMod][0] === 1){
         // console.log('moving into occupied green space')
+
         //if the space about to be occupied by an eater is currently occupied by a green,
         //remove the green from the canvas.
         let greenId = `${x+xMod},${y+yMod}`
@@ -342,6 +429,9 @@ const moveEater = (eater) => {
         // console.log(greensIndex)
         greens.splice(greensIndex, 1)
         app.stage.removeChild(green)
+
+        //Add a point of nutrition for the eater that removed the green
+        eater.nutrients = eater.nutrients+1
         
     }
 
@@ -367,30 +457,58 @@ const moveEater = (eater) => {
 }
 
 const reproduceEater = (eater) => {
-    //if eater is within the middle third of its total lifespan, roll a small chance for reproduction
-    if (eater.age > (eaterLifespan*(1/3)) && eater.age < (eaterLifespan*(2/3))) {
-        if (Math.random() < eaterReproductionRate) {
+    //first determine if eater is currently gestating, if not, roll for age and gestation. If it is, manage gestation period 
 
-            
+    // console.log('reproducing function running')
+    if (eater.gestation === undefined) {
+        //if eater is within the middle third of its total lifespan and has collected sufficient nutrients, roll a small chance for reproduction
+        if (eater.age > (eaterLifespan*(1/3)) && eater.age < (eaterLifespan*(2/3)) && eater.nutrients > eaterNutrientRequirement) {
+            // console.log('able to reproduce')
+            if (Math.random() < eaterReproductionRate) {
+                
+                //if sucessful splitting is possible, make the eater remain in the same location until gestation period reaches zero
+                eater.gestation = eaterGestationPeriod
+                //Remove nutrients from eater 
+                eater.nutrients = eater.nutrients - eaterNutrientRequirement
+            }
         }
+     } else if (eater.gestation > 0) {
+        // console.log(eater.gestation)
+        eater.gestation = eater.gestation - 1
+        eater.tint = "0x228B22"
+    } else if (eater.gestation === 0) {
+        //birth child and set gestation to undefined
+        // console.log('child born')
 
+        eater.tint = '0xFFFFFF'
+        let childLocation = scanAreaForNewEater(eater.id)
 
+        // console.log(childLocation)
+
+        drawEater(childLocation[0],childLocation[1])
+
+        eater.gestation = undefined
     }
+
+
 
 }
 
-//Given an eater sprite object, scan the area and choose a target for the eater. If no target is found, set target attribute to = ''.
+//Given an eater sprite object, scan the area and choose a target for the eater. If no target is found, set target attribute to undefined.
 const searchForPrey = (eater) => {
     
     //determine if the given eater has a target, if not, scan the area and assign the eater target a target if one is found in the area.
     //If none are found in the area, the eater will default to random movement 
 
     if (eater.target === undefined){
-        let prey = scanArea(eater.id, eaterPerception, 1)
-        // console.log(prey.id)
-        if (prey !== undefined){
-            console.log(prey)
-            eater.target = prey.id
+        let prey = scanAreaForGreens(eater.id, eaterPerception)
+        // console.log(prey)
+
+        if (prey.length !== 0){
+            
+            let target = prey[Math.floor(Math.random()*prey.length)]
+            eater.target = target.id
+            // eater.target = prey.id
             // eater.target = prey[Math.floor(Math.random()*prey.length)-1].id
             // console.log(prey)
             // console.log(eater.target)
@@ -419,38 +537,53 @@ const startSim = () => {
 
 }
 
+const updateStats = () => {
 
-const gameLoop = () => {
+// const greenPop = document.getElementById('green-pop')
+// const eaterPop = document.getElementById('eater-pop')
+// const greenEaterRatio = document.getElementById('green-eater-ratio')
+// const averageGen = document.getElementById('average-eater-generation')
+
+    greenPop.innerText = `Green Population: ${greens.length}`
+    eaterPop.innerText = `Eater Population: ${eaters.length}`
+    greenEaterRatio.innerText = `Green to Eater Ratio: ${Math.round(greens.length / eaters.length)}:1`
     
-    if (running === true){
+    
+
+}
+
+const checkForExtinction = () => {
+    if (greens.length === 0) {
+        // pauseSim()
+        if (messages.length === 0){
+            const doomMessage = new PIXI.Text('Greens have gone extinct. As a result, all eaters will starve.', messageStyle)
+            doomMessage.position.set(width/4, height/2)
+            messages.push(doomMessage)
+            app.stage.addChild(doomMessage)
+        }
         
+
+        if (eaters.length === 0) {
+            pauseSim()
+            resumeButton.disabled = true
+
+
+            messages[0].text = 'Greens have gone extinct. As a result, all eaters have starved.'
+        }
         
-        //Iterate over eater object
-        eaters.forEach(function(eater){
-            // console.log(eater.id[0])
-            // console.log(eater.id[2])
-            
-            
-            searchForPrey(eater);
-            moveEater(eater);
-            manageAge(eater)
-            reproduceEater(eater)
-            
-        })
-        
-        //Iterate over each green object
+    } else if (eaters.length === 0) {
+        if (messages.length === 0) {
+            const doomMessage = new PIXI.Text('Eaters have gone extinct. As a result, Greens will dominate the tidepool unchecked.', messageStyle)
+            doomMessage.position.set(width/4, height/2)
+            messages.push(doomMessage)
+            app.stage.addChild(doomMessage)
+        }
+        if (greens.length === (numCols*numRows)) {
+            pauseSim()
+            resumeButton.disabled = true
 
-        //ADVANTAGE - we are no longer iterating over the entire grid each step, now we are only iterating over the arrays containing the elements
-        let gridSnapShot = [...grid]
-        greens.forEach(function(green){
-
-            let x = green.id.split(',')[0]
-            let y = green.id.split(',')[1]
-
-            try { reproduceGreen(parseInt(x), parseInt(y), `${x},${y}`) }
-            catch (e) {}//console.log(e)}
-
-        })
+            messages[0].text = 'Eaters have gone extinct. As a result, Greens have dominated the tidepool unchecked.'
+        }
     }
 }
 
@@ -490,9 +623,49 @@ const addButtonListeners = () => {
     })
 }
 
+const gameLoop = () => {
+    
+    if (running === true){
+        
+        
+        //Iterate over eater object
+        eaters.forEach(function(eater){
+            // console.log(eater.id[0])
+            // console.log(eater.id[2])
+            
+            
+            searchForPrey(eater);
+            moveEater(eater);
+            manageAge(eater)
+            reproduceEater(eater)
+            
+        })
+        
+        //Iterate over each green object
+
+        //ADVANTAGE - we are no longer iterating over the entire grid each step, now we are only iterating over the arrays containing the elements
+        let gridSnapShot = [...grid]
+        greens.forEach(function(green){
+
+            let x = green.id.split(',')[0]
+            let y = green.id.split(',')[1]
+
+            try { reproduceGreen(parseInt(x), parseInt(y), `${x},${y}`) }
+            catch (e) {}//console.log(e)}
+
+        })
+
+        checkForExtinction()
+        updateStats()
+
+    }
+}
+
+
 //Main config 
 const mainConfig = () => {
     
+
     canvasDiv.appendChild(app.view);
 
     gridConfig.addEventListener('submit', function(event){
@@ -502,6 +675,8 @@ const mainConfig = () => {
         populateGreens(initalGreenPopInput.value)
 
         populateEaters(initalEaterPopInput.value)
+
+
 
         startSim()
         
@@ -520,6 +695,7 @@ document.addEventListener('DOMContentLoaded', function(event){
     grid = generateGrid(gridScale)
     
     // drawGrid()
+    // drawBackgroundSprite()
     
     mainConfig()
     
